adc2598c0ffe52ed984f0f9c1db22439f532a2a4
Sayalikukkar <sayukukkar11@gmail.com>
2023-04-02 14:16:14 +0530

    org & channle creation

--- test-network/scripts/setAnchorPeer.sh
+++ test-network/scripts/setAnchorPeer.sh
@@ -26,6 +26,12 @@ createAnchorPeerUpdate() {
   elif [ $ORG -eq 3 ]; then
     HOST="peer0.org3.example.com"
     PORT=11051
+  elif [ $ORG -eq 3 ]; then
+    HOST="peer0.org4.example.com"
+    PORT=12051
+  elif [ $ORG -eq 3 ]; then
+    HOST="peer0.org5.example.com"
+    PORT=13051
   else
     errorln "Org${ORG} unknown"
   fi
